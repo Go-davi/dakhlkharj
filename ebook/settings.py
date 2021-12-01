3ca3bfbd2fd7d3493f7a94ba24840e22094fcc79
david <david>
2021-12-01 13:29:27 +0000

    remove security code

--- ebook/settings.py
+++ ebook/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=q6nt6j(a+x0lcc42s6_(+f=w=5r)g=iegi%_3mhxlcs&k^0)m'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
